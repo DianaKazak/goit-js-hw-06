73d6b15be30ee695c826b1385327f579b2212698
DianaKazak <dianakazak20020926@gmail.com>
2022-12-24 23:28:16 +0200

    все

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsList = document.querySelector("#ingredients");
+ingredientsList.append(
+  ...ingredients.map((item) => {
+    let itemList = document.createElement("li");
+    itemList.textContent = item;
+    return itemList;
+  })
+);
