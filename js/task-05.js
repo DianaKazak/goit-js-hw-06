2c4363dfe2fe30f69fb1c210f4ea24563a78f10f
DianaKazak <dianakazak20020926@gmail.com>
2022-12-31 00:19:47 +0200

    //

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,13 @@
-const textInput = document.querySelector('#name-input')
-const output = document.querySelector('#name-output')
+const input = document.querySelector('#name-input');
 
-textInput.addEventListener('input', event => {
-	output.textContent = event.target.value
+const output = document.querySelector('#name-output');
 
-	if (event.target.value === '') {
-		output.textContent = 'Anonymous'
-	}
-})
+input.addEventListener('input', OnInputChange);
+
+function OnInputChange(event) {
+output.textContent = event.currentTarget.value;
+
+if (event.currentTarget.value.trim() === '') {
+output.textContent = 'Anonymous';
+}
+}
--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,36 @@
+//const form = document.querySelector('.login-form')
+
+//form.addEventListener('submit', handleSubmit)
+
+//function handleSubmit(event) {
+//    event.preventDefault()
+//    const {
+//        elements: { email, password },
+//    } = event.currentTarget
+//
+  //  if (email.value === '' || password.value === '') {
+    //    return alert('Please fill in all the fields!')
+   // }
+   // const userDetails = { email: email.value, Password: password.value }
+  //
+  //  console.log(userDetails)
+ //   event.currentTarget.reset()
+//}
+
 const form = document.querySelector('.login-form')
 
 form.addEventListener('submit', handleSubmit)
 
 function handleSubmit(event) {
-    event.preventDefault()
-    const {
-        elements: { email, password },
-    } = event.currentTarget
+event.preventDefault()
+const { elements: { email, password },
+} = event.currentTarget
 
-    if (email.value === '' || password.value === '') {
-        return alert('Please fill in all the fields!')
-    }
-    const userDetails = { email: email.value, Password: password.value }
+if (email.value === '' || password.value === '') {
+return alert('Заповніть всі поля')
+}
+const userDetails = { email: email.value, Password: password.value }
     
-    console.log(userDetails)
-    event.currentTarget.reset()
+console.log(userDetails)
+event.currentTarget.reset()
 }
